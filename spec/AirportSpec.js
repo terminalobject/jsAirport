f19fb2018f9c07ec896e0dc3a12b6881eefe2906
terminalobject <doctorboredom@hotmail.com>
2017-07-04 12:42:11 +0100

    second test passed

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,9 +6,14 @@ describe ("an airport", function(){
     plane = 'boeing747'
   })
   
-  it("can land a plane", function() {
+  it("can land a plane and confirm that it landed", function() {
     airport.land(plane)
     expect(airport.planes).toContain(plane);  
-  })  
-
+    expect(airport.land(plane)).toEqual("Plane landed.")
+  })
+  it("can instruct a plane to take off and confirm it did", function(){
+    airport.takeoff(plane)
+    expect(airport.planes).not.toContain(plane);
+    expect(airport.takeoff(plane)).toEqual("Plane took off.")
+  })
 })
--- src/Airport.js
+++ src/Airport.js
@@ -3,6 +3,9 @@ function Airport() {
 }
 Airport.prototype.land = function(plane) {
   this.planes.push(plane);
-  return this.planes;
+  return "Plane landed." 
 }
-
+Airport.prototype.takeoff = function(plane) {
+  this.planes.splice(this.planes.indexOf(plane));
+  return "Plane took off."
+}
